9f95724e73a8b22544df537a1beb0ae8a77d0568
Martina <martina.ria@hotmail.com>
2020-04-18 16:34:53 +0200

    Architettura base html

---
+++ Ex/main.js
@@ -0,0 +1,27 @@
+/*DESCRIZIONE:
+- Griglia 6x6,
+- ad ogni click (su ogni rettangolino) parte una richiesta AJAX che prende un numero random da 1 a 9 (primo end-point della API in slide).
+- Se il num ritornato è <= 5 il quadrato diventa giallo,
+- se è > di 5 il quadrato diventa verde.
+- Il numero ottenuto appare al centro del quadrato
+
+BONUS:
+- se utente clicca 2 volte sullo stesso rettangolo lo mando a ca… gli dico che non si può;
+- generare dinamicamente la griglia dei quadrati;
+- Curo per bene l’output, creando un layout carino;
+- varie che vi vengono in mente per sperimentare;
+
+NOTE:
+la griglia per la richiesta base è statica, quindi scritta a mano da me in html;
+come sempre i bonus vanno in sottocartelle. */
+
+
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+
+
+
+});
